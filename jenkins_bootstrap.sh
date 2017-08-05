fab5c7f59c3b4f977353e90195bfbc29c860ab19
Sam Gleske <sam.mxracer@gmail.com>
2017-08-04 17:36:19 -0700

    Split out packaging and variables
    Also updated jenkins_bootstrap.sh to automatically update
    SCRIPT_LIBRARY_PATH based on whether or not the bootstrap scripts are in
    a submodule.

--- jenkins_bootstrap.sh
+++ jenkins_bootstrap.sh
@@ -27,8 +27,13 @@ export JENKINS_WAR="${JENKINS_WAR:-jenkins.war}"
 export JENKINS_HOME="${JENKINS_HOME:-../my_jenkins_home}"
 export JENKINS_START="${JENKINS_START:-java -Xms4g -Xmx4g -XX:MaxPermSize=512M -jar ${JENKINS_WAR}}"
 export JENKINS_WEB="${JENKINS_WEB:-http://localhost:8080}"
-export SCRIPT_LIBRARY_PATH="${SCRIPT_LIBRARY_PATH:-${BOOTSTRAP_HOME}/scripts}"
 export jenkins_url="${jenkins_url:-http://mirrors.jenkins-ci.org/war/latest/jenkins.war}"
+
+if [ -d 'jenkins-bootstrap-shared' ]; then
+  export SCRIPT_LIBRARY_PATH="${SCRIPT_LIBRARY_PATH:-${BOOTSTRAP_HOME}/jenkins-bootstrap-shared/scripts}"
+else
+  export SCRIPT_LIBRARY_PATH="${SCRIPT_LIBRARY_PATH:-${BOOTSTRAP_HOME}/scripts}"
+fi
 
 if [ -e "${SCRIPT_LIBRARY_PATH}/common.sh" ]; then
   source "${SCRIPT_LIBRARY_PATH}/common.sh"
