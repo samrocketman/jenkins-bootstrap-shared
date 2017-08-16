3f43e90616bb2048f623420026f1ca163b194f4f
Sam Gleske <sam.mxracer@gmail.com>
2017-08-15 20:59:56 -0700

    lower upgrade verbosity

--- scripts/upgrade/env.sh
+++ scripts/upgrade/env.sh
@@ -9,7 +9,7 @@ if [ -f build.gradle ]; then
   export JENKINS_PASSWORD="${JENKINS_PASSWORD:-$(<"${JENKINS_HOME}"/secrets/initialAdminPassword)}"
   unset JENKINS_CALL_ARGS
   jenkins-call-url -a -v -v "${JENKINS_WEB}"/api/json -o /dev/null
-  export JENKINS_CALL_ARGS="-m POST -v ${JENKINS_WEB}/scriptText --data-string script= -d"
+  export JENKINS_CALL_ARGS="-m POST ${JENKINS_WEB}/scriptText --data-string script= -d"
 else
   echo "Not in repository root." 1>&2
 fi
