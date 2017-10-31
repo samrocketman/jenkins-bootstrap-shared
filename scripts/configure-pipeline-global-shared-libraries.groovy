179617a390104c7ce7a8b6321c62a42c8f53a719
Sam Gleske <sam.mxracer@gmail.com>
2017-10-30 19:04:49 -0700

    Script console script to configure pipeline shared
    libraries.  This is for configuring global shared libraries.

---
+++ scripts/configure-pipeline-global-shared-libraries.groovy
@@ -0,0 +1,113 @@
+/*
+   Copyright 2015-2017 Sam Gleske - https://github.com/samrocketman/jenkins-bootstrap-jervis
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+   */
+/*
+   Configure pipline shared libraries in the global Jenkins configuration.
+   This will safely compare configured libraries and only overwrite the global
+   shared library config if changes have been made.
+
+   workflow-cps-global-lib 2.9
+ */
+
+import jenkins.plugins.git.GitSCMSource
+import jenkins.plugins.git.traits.BranchDiscoveryTrait
+import org.jenkinsci.plugins.workflow.libs.GlobalLibraries
+import org.jenkinsci.plugins.workflow.libs.LibraryConfiguration
+import org.jenkinsci.plugins.workflow.libs.SCMSourceRetriever
+import net.sf.json.JSONObject
+
+/**
+  Function to compare if the two global shared libraries are equal.
+ */
+boolean librariesEqual(List lib1, List lib2) {
+  //compare returns true or false
+  lib1.size() == lib2.size() &&
+  !(
+    false in [lib1, lib2].transpose().collect { l1, l2 ->
+      def s1 = l1.retriever.scm
+      def s2 = l2.retriever.scm
+      l1.retriever.class == l2.retriever.class &&
+      l1.name == l2.name &&
+      l1.defaultVersion == l2.defaultVersion &&
+      l1.implicit == l2.implicit &&
+      l1.allowVersionOverride == l2.allowVersionOverride &&
+      l1.includeInChangesets == l2.includeInChangesets &&
+      s1.remote == s2.remote &&
+      s1.credentialsId == s2.credentialsId &&
+      s1.traits.size() == s2.traits.size() &&
+      !(
+        false in [s1.traits, s2.traits].transpose().collect { t1, t2 ->
+          t1.class == t2.class
+        }
+      )
+    }
+  )
+}
+
+/* Example configuration
+pipeline_shared_libraries = [
+    'Global Library Name': [
+        'defaultVersion': 'master-fu',
+        'implicit': true,
+        'allowVersionOverride': false,
+        'includeInChangesets': false,
+        'scm': [
+            'remote': 'git@github.com:example/project.git',
+            'credentialsId': 'your-credentials-id'
+        ]
+    ]
+]
+ */
+
+if(!binding.hasVariable('pipeline_shared_libraries')) {
+    pipeline_shared_libraries = [:]
+}
+
+if(!pipeline_shared_libraries in Map) {
+}
+
+pipeline_shared_libraries = pipeline_shared_libraries as JSONObject
+
+List libraries = [] as ArrayList
+pipeline_shared_libraries.each { name, config ->
+    if(name && config && config in Map && 'scm' in config && config['scm'] in Map && 'remote' in config['scm'] && config['scm'].optString('remote')) {
+        def scm = new GitSCMSource(config['scm'].optString('remote'))
+        scm.credentialsId = config['scm'].optString('credentialsId')
+        scm.traits = [new BranchDiscoveryTrait()]
+        def retriever = new SCMSourceRetriever(scm)
+        def library = new LibraryConfiguration(name, retriever)
+        library.defaultVersion = config.optString('defaultVersion')
+        library.implicit = config.optBoolean('implicit', false)
+        library.allowVersionOverride = config.optBoolean('allowVersionOverride', true)
+        library.includeInChangesets = config.optBoolean('allowVersionOverride', true)
+        libraries << library
+    }
+}
+
+def global_settings = Jenkins.instance.getExtensionList(GlobalLibraries.class)[0]
+
+if(libraries && !librariesEqual(global_settings.libraries, libraries)) {
+    global_settings.libraries = libraries
+    global_settings.save()
+    print 'Configured Pipeline Global Shared Libraries:\n    ' + global_settings.libraries.collect { it.name }.join('\n    ')
+}
+else {
+    if(libraries) {
+        println 'Nothing changed.  Pipeline Global Shared Libraries already configured.'
+    }
+    else {
+        println 'Nothing changed.  Pipeline Global Shared Libraries empty.'
+    }
+}
