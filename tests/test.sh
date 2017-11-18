96e0cc3e91a7b6a2ea5889f9fade7d1a2f14c3f2
Sam Gleske <sam.mxracer@gmail.com>
2017-11-17 17:04:07 -0800

    Update some integration tests

--- tests/test.sh
+++ tests/test.sh
@@ -27,33 +27,17 @@ set -x
 bash -n ./tests/random_port.sh
 test -x ./tests/random_port.sh
 export RANDOM_PORT="$(./tests/random_port.sh)"
-bash -n ./jervis_bootstrap.sh
-test -x ./jervis_bootstrap.sh
+bash -n ./jenkins_bootstrap.sh
+test -x ./jenkins_bootstrap.sh
 export JENKINS_START="java -jar jenkins.war --httpPort=${RANDOM_PORT} --httpListenAddress=127.0.0.1"
 export JENKINS_WEB="http://127.0.0.1:${RANDOM_PORT}"
 export JENKINS_CLI="java -jar ./jenkins-cli.jar -s http://127.0.0.1:${RANDOM_PORT} -noKeyAuth"
-export JENKINS_HOME="$(mktemp -d ../my_jenkins_homeXXX)"
-./jervis_bootstrap.sh
+export JENKINS_HOME="$(mktemp -d /tmp/my_jenkins_homeXXX)"
 set +x
-
-#Jenkins 2.0
-source scripts/common.sh
-CURL='curl'
-#try enabling authentication
-if is_auth_enabled; then
-  export CURL="${CURL} -u admin:$(<${JENKINS_HOME}/secrets/initialAdminPassword)"
-fi
-#try enabling CSRF protection support
-csrf_set_curl
-set -x
-
-$CURL -s "${JENKINS_WEB}/api/json?pretty=true" | python ./tests/api_test.py
-$CURL -X POST "${JENKINS_WEB}/job/_jervis_generator/build?delay=0sec" --data-urlencode json='{"parameter": [{"name":"project", "value":"samrocketman/jervis"}, {"name":"branch", "value":""}]}'
-sleep 1
-python ./tests/job_test.py "${JENKINS_WEB}/job/_jervis_generator/lastBuild/api/json?pretty=true"
-test -e "$JENKINS_HOME"/jobs/samrocketman/jobs/jervis-master/config.xml
+./jenkins_bootstrap.sh
 test -e jenkins.pid
-./scripts/provision_jenkins.sh stop
+./scripts/provision_jenkins.sh url-ready "${JENKINS_WEB}/jnlpJars/jenkins-cli.jar"
+bash -x ./scripts/provision_jenkins.sh stop
 test ! -e jenkins.pid
 test -e console.log
 test -e jenkins.war
@@ -64,3 +48,6 @@ test ! -e console.log
 test ! -e jenkins.war
 test ! -e "$JENKINS_HOME"
 test ! -e plugins
+
+# check for bash syntax
+find . -type f -name '*.sh' | xargs -n1 bash -n
