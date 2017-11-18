e9e32b317ad5317ffd93e64c48fb3e5cf8a7345f
Sam Gleske <sam.mxracer@gmail.com>
2017-11-17 19:43:26 -0800

    Validate no longer installs packages

--- tests/rpm/validate.sh
+++ tests/rpm/validate.sh
@@ -3,13 +3,13 @@
 
 set -euo pipefail
 
-PACKAGES=(java-1.8.0-openjdk-devel git)
 GOSS_SHASUM=5669df08e406abf594de0e7a7718ef389e5dc7cc76905e7f6f64711e6aad7fa3
 GOSS_URL=https://github.com/aelsabbahy/goss/releases/download/v0.3.5/goss-linux-amd64
 
 #install packages
-rpm -q "${PACKAGES[@]}" || yum install -y "${PACKAGES[@]}"
-export JAVA_HOME=/usr/lib/jvm/java-1.8.0
+#PACKAGES=(java-1.8.0-openjdk-devel git)
+#rpm -q "${PACKAGES[@]}" || yum install -y "${PACKAGES[@]}"
+#export JAVA_HOME=/usr/lib/jvm/java-1.8.0
 
 # build rpm
 [ -f build/distributions/*.rpm ] || ./gradlew buildRpm
