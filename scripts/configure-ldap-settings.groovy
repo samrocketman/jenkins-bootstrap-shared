12eef2f6e17dcf1c02c5ba5c2778cae73a2a7a93
Sam Gleske <sam.mxracer@gmail.com>
2018-02-05 01:05:16 -0800

    2018 license bump

--- scripts/configure-ldap-settings.groovy
+++ scripts/configure-ldap-settings.groovy
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2015-2017 Sam Gleske - https://github.com/samrocketman/jenkins-bootstrap-shared
+   Copyright (c) 2015-2018 Sam Gleske - https://github.com/samrocketman/jenkins-bootstrap-shared
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
