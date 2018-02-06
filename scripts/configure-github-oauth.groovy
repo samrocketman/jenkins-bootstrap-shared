c1916afc7c03b3bb8f1e2670baca85d6ba651e8a
Sam Gleske <sam.mxracer@gmail.com>
2018-02-06 00:48:39 -0800

    Script: Configure GitHub security realm
    Configures the github-oauth plugin to use GitHub as a security realm.

---
+++ scripts/configure-github-oauth.groovy
@@ -0,0 +1,58 @@
+/*
+   Copyright (c) 2015-2018 Sam Gleske - https://github.com/samrocketman/jenkins-bootstrap-jervis
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+   */
+
+/*
+   Configures GitHub as the security realm from the GitHub Authentication
+   Plugin (github-oauth).
+
+   github-oauth 0.29
+ */
+
+import hudson.security.SecurityRealm
+import org.jenkinsci.plugins.GithubSecurityRealm
+import net.sf.json.JSONObject
+
+if(!binding.hasVariable('github_realm')) {
+    github_realm = [:]
+}
+
+if(!(github_realm instanceof Map)) {
+    throw new Exception('github_realm must be a Map.')
+}
+
+github_realm = github_realm as JSONObject
+
+String githubWebUri = github_realm.optString('web_uri', GithubSecurityRealm.DEFAULT_WEB_URI)
+String githubApiUri = github_realm.optString('api_uri', GithubSecurityRealm.DEFAULT_API_URI)
+String oauthScopes = github_realm.optString('oauth_scopes', GithubSecurityRealm.DEFAULT_OAUTH_SCOPES)
+String clientID = github_realm.optString('client_id')
+String clientSecret = github_realm.optString('client_secret')
+
+if(!Jenkins.instance.isQuietingDown()) {
+    if(clientID && clientSecret) {
+        SecurityRealm github_realm = new GithubSecurityRealm(githubWebUri, githubApiUri, clientID, clientSecret, oauthScopes)
+        //check for equality, no need to modify the runtime if no settings changed
+        if(!github_realm.equals(Jenkins.instance.getSecurityRealm())) {
+            Jenkins.instance.setSecurityRealm(github_realm)
+            Jenkins.instance.save()
+            println 'Security realm configuration has changed.  Configured GitHub security realm.'
+        } else {
+            println 'Nothing changed.  GitHub security realm already configured.'
+        }
+    }
+} else {
+    println 'Shutdown mode enabled.  Configure GitHub security realm SKIPPED.'
+}
