ceca2b462f5a9ab4734eadbef4559ad374ca3186
Sam Gleske <sam.mxracer@gmail.com>
2018-02-17 20:20:35 -0800

    Bugfix initial password not available
    on first time vagrant boot.

--- scripts/vagrant-env.sh
+++ scripts/vagrant-env.sh
@@ -1,3 +1,7 @@
 export VAGRANT_SSH_CONFIG=$(mktemp)
 vagrant ssh-config > "$VAGRANT_SSH_CONFIG"
+while ! ssh -nF "${VAGRANT_SSH_CONFIG}" default 'sudo test -f /var/lib/jenkins/secrets/initialAdminPassword'; do
+  echo '/var/lib/jenkins/secrets/initialAdminPassword not available, yet...'
+  sleep 5
+done
 export JENKINS_PASSWORD=$(ssh -F "${VAGRANT_SSH_CONFIG}" default 'sudo cat /var/lib/jenkins/secrets/initialAdminPassword')
