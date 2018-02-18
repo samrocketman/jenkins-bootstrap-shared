beaf5e28825d7e5119ed65c5c73ea32c6ae5cd21
Sam Gleske <sam.mxracer@gmail.com>
2018-02-17 23:01:13 -0800

    Don't overwrite password with docker

--- scripts/docker-env.sh
+++ scripts/docker-env.sh
@@ -4,4 +4,6 @@ if ! type -P docker-compose || [ -z "$(docker-compose ps -q)" ]; then
   exit 1
 fi
 
-export JENKINS_PASSWORD=$(docker-compose ps -q | xargs -n1 -I {} docker exec -t {} cat /var/lib/jenkins/secrets/initialAdminPassword | sed 's/\r$//')
+if [ -z "${JENKINS_PASSWORD}" ]; then
+  export JENKINS_PASSWORD=$(docker-compose ps -q | xargs -n1 -I {} docker exec -t {} cat /var/lib/jenkins/secrets/initialAdminPassword | sed 's/\r$//')
+fi
