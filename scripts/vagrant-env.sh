0b92cf5a4f7e319c4cae22a626082503071c81c7
Sam Gleske <sam.mxracer@gmail.com>
2018-06-10 17:26:33 -0700

    Vagrant support for customized Jenkins home dir

--- scripts/vagrant-env.sh
+++ scripts/vagrant-env.sh
@@ -1,9 +1,26 @@
 export VAGRANT_SSH_CONFIG=$(mktemp)
 vagrant ssh-config > "$VAGRANT_SSH_CONFIG"
+function get_jenkins_home() {
+  ssh -F "${VAGRANT_SSH_CONFIG}" default /bin/bash <<'EOF'
+CONFIG_FILE=""
+if sudo test -f /etc/sysconfig/jenkins && sudo grep '^JENKINS_HOME' /etc/sysconfig/jenkins &> /dev/null; then
+  CONFIG_FILE='/etc/sysconfig/jenkins'
+elif sudo test -f /etc/default/jenkins && sudo grep '^JENKINS_HOME' /etc/default/jenkins &> /dev/null; then
+  CONFIG_FILE='/etc/default/jenkins'
+fi
+if test -n "${CONFIG_FILE}"; then
+  eval "$(sudo grep '^JENKINS_HOME' "${CONFIG_FILE}")"
+fi
+echo "${JENKINS_HOME:-/var/lib/jenkins}"
+EOF
+}
+
+VAGRANT_JENKINS_HOME="$(get_jenkins_home)"
+
 if [ -z "${JENKINS_PASSWORD}" ]; then
-  while ! ssh -nF "${VAGRANT_SSH_CONFIG}" default 'sudo test -f /var/lib/jenkins/secrets/initialAdminPassword'; do
+  while ! ssh -nF "${VAGRANT_SSH_CONFIG}" default "sudo test -f \"${VAGRANT_JENKINS_HOME}\"/secrets/initialAdminPassword"; do
     echo '/var/lib/jenkins/secrets/initialAdminPassword not available, yet...'
     sleep 5
   done
-  export JENKINS_PASSWORD=$(ssh -F "${VAGRANT_SSH_CONFIG}" default 'sudo cat /var/lib/jenkins/secrets/initialAdminPassword')
+  export JENKINS_PASSWORD=$(ssh -F "${VAGRANT_SSH_CONFIG}" default "sudo cat \"${VAGRANT_JENKINS_HOME}\"/secrets/initialAdminPassword")
 fi
