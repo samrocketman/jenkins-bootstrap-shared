5a4464876424d6c3ea29f4a77acc6500e356b6ac
Sam Gleske <sam.mxracer@gmail.com>
2018-06-29 13:41:32 -0700

    Bugfix portability of docker-env.sh script
    Using the TTY means that the password included a carriage return.  This
    was because of the `-t` option and not specific to docker.
    
    See also:
    
    - https://github.com/moby/moby/issues/37366
    - https://github.com/moby/moby/issues/8513#issuecomment-216191236

--- scripts/docker-env.sh
+++ scripts/docker-env.sh
@@ -1,9 +1,21 @@
+#!/bin/bash
 
 if ! type -P docker-compose || [ -z "$(docker-compose ps -q)" ]; then
   echo "ERROR: docker-compose does not exist or is not running" >&2
   exit 1
 fi
 
+function password_ready() {
+  docker-compose ps -q | \
+  xargs -n1 -I {} docker exec {} test -r /var/lib/jenkins/secrets/initialAdminPassword
+}
+
+
 if [ -z "${JENKINS_PASSWORD}" ]; then
-  export JENKINS_PASSWORD=$(docker-compose ps -q | xargs -n1 -I {} docker exec -t {} cat /var/lib/jenkins/secrets/initialAdminPassword | sed 's/\r$//')
+  until password_ready; do
+    echo '/var/lib/jenkins/secrets/initialAdminPassword not available, yet...'
+    sleep 5
+  done
+  JENKINS_PASSWORD="$(docker-compose ps -q | xargs -n1 -I {} docker exec {} cat /var/lib/jenkins/secrets/initialAdminPassword)"
+  export JENKINS_PASSWORD
 fi
