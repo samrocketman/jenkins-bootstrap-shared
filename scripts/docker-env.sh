58edd1a70a731017fdb7ff40bdd9526656c58d0f
Sam Gleske <sam.mxracer@gmail.com>
2018-09-13 00:26:59 -0700

    Upgrade support for docker jenkins home custom path

--- scripts/docker-env.sh
+++ scripts/docker-env.sh
@@ -7,15 +7,15 @@ fi
 
 function password_ready() {
   docker-compose ps -q | \
-  xargs -n1 -I {} docker exec {} test -r /var/lib/jenkins/secrets/initialAdminPassword
+  xargs -n1 -I {} docker exec {} test -r "${DOCKER_JENKINS_HOME:-/var/lib/jenkins}"/secrets/initialAdminPassword
 }
 
 
 if [ -z "${JENKINS_PASSWORD}" ]; then
   until password_ready; do
-    echo '/var/lib/jenkins/secrets/initialAdminPassword not available, yet...'
+    echo "${DOCKER_JENKINS_HOME:-/var/lib/jenkins}/secrets/initialAdminPassword not available, yet..."
     sleep 5
   done
-  JENKINS_PASSWORD="$(docker-compose ps -q | xargs -n1 -I {} docker exec {} cat /var/lib/jenkins/secrets/initialAdminPassword)"
+  JENKINS_PASSWORD="$(docker-compose ps -q | xargs -n1 -I {} docker exec {} cat "${DOCKER_JENKINS_HOME:-/var/lib/jenkins}"/secrets/initialAdminPassword)"
   export JENKINS_PASSWORD
 fi
